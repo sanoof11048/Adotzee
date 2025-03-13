46d486bb605369d4f8590915b89195e7fdda0933
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-03-13 10:26:15 +0530

    Delete src directory

--- src/components/LazyImages/FooterLogo.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import foot from '../../assets/outSide.png'
-
-    const FooterLogo = () => <img src={foot} alt="Hat Logo" className="md:w-40 md:h-40 w-20 h-20" loading="lazy" />;
-
-
-export default FooterLogo
--- src/components/LazyImages/HatImage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import hat from "../../assets/hat.png";
-
-const HatImage = () => <img src={hat} alt="Hat Logo" className="w-18 h-18 -m-2 hidden md:block" loading="lazy" />;
-
-export default HatImage;
